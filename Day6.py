85900f1d4a0115ac8124f6b8ba827f5b4a3984ec
Ralf <ravalj@ttu.ee>
2022-12-06 17:39:51 +0200

    day 6 part 2

--- Day6.py
+++ Day6.py
@@ -3,14 +3,15 @@ datastream = None
 with open("Day6.txt") as f:
     datastream = f.readline()
 
-def find_start_of_packet_marker(data: str):
+def find_start_of_packet_marker(data: str, length=4):
     marker_end = None
 
-    for end in range(4, len(data)):
-        marker = set(data[end-4:end])
-        if len(marker) == 4:
+    for end in range(length, len(data)):
+        marker = set(data[end-length:end])
+        if len(marker) == length:
             return end
 
     return marker_end
 
 print(find_start_of_packet_marker(datastream))
+print(find_start_of_packet_marker(datastream, 14))
