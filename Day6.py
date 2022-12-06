33c5629522b6df46532fb70d7915ca962cc5a138
Ralf <ravalj@ttu.ee>
2022-12-06 17:43:46 +0200

    reduce length of function

--- Day6.py
+++ Day6.py
@@ -4,14 +4,10 @@ with open("Day6.txt") as f:
     datastream = f.readline()
 
 def find_start_of_packet_marker(data: str, length=4):
-    marker_end = None
-
-    for end in range(length, len(data)):
-        marker = set(data[end-length:end])
+    for marker_end in range(length, len(data)):
+        marker = set(data[marker_end-length:marker_end])
         if len(marker) == length:
-            return end
-
-    return marker_end
+            return marker_end
 
 print(find_start_of_packet_marker(datastream))
 print(find_start_of_packet_marker(datastream, 14))
